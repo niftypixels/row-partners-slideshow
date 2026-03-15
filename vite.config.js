242c75664a5f3901c716b2bf2a883c4897c7ddba
Scott Twede <scott@niftypixels.com>
2026-03-15 16:20:10 -0700

    chore: remove React dependency

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 import { name } from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: `/${name}/`,
   mode: process.env.CLOUDFLARE ? 'cloudflare' : 'production',
-  plugins: [react()],
   build: {
     rollupOptions: {
       external: ['gsap'],
       output: {
+        format: 'iife',
         globals: {
           'gsap': 'gsap'
         }
